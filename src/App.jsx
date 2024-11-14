d538b0e4427924664c058277a97091511018137f
Soy-Zetta <magaviria19@soy.sena.edu.co>
2024-11-13 20:01:49 -0500

    añadi modificacion de temas

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@
 import { MyRoutes } from "./index";
 import {createContext, useState} from "react"
 import {Light,Dark} from "./index"
+import {ThemeProvider} from "styled-components"
 export const ThemeContext = createContext(null);
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState ("light");
   const themeStyle = theme === "light"? Light:Dark
   return (
     <>
